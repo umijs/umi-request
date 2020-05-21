4522b07f3cade11f548e7d3c90bdb9a75f41d021
bxer <yishidixiashang@gmail.com>
2020-05-21 20:03:20 +0800

    feat: 添加abort controller 取消请求 (#90)
    Co-authored-by: 温鑫 <xin.wen@baifendian.com>

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import request, { extend, fetch } from './request';
 import Onion from './onion';
 import { RequestError, ResponseError } from './utils';
+import AbortController from './cancel/abortControllerCancel';
 
-export { extend, RequestError, ResponseError, Onion, fetch };
+export { extend, RequestError, ResponseError, Onion, fetch, AbortController };
 export default request;
