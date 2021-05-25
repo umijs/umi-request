17867b5db4d6692b2dce40544b4a2e1171052e75
Troy Li <troy.lty@alipay.com>
2021-05-25 19:21:30 +0800

    chore: remove abort controller polyfill (#251)
    * chore: remove abort controller polyfill
    
    * test: update test case and remove AbortController export
    
    * docs: update doc for abort controller

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import request, { extend, fetch } from './request';
 import Onion from './onion';
 import { RequestError, ResponseError } from './utils';
-import { AbortController, AbortSignal } from './cancel/abortControllerCancel';
 
-export { extend, RequestError, ResponseError, Onion, fetch, AbortController, AbortSignal };
+export { extend, RequestError, ResponseError, Onion, fetch };
 
 export default request;
